010ce764a37ecbeccdc05adf55a8fd199f4ac7dd
Jose Villa <j0542420@amarillocollege.com>
2025-05-07 16:42:23 -0500

    using Javascript

--- project12-02.js
+++ project12-02.js
@@ -10,6 +10,7 @@
       Filename: project12-02.js
 */
 
+// using jQuery
 $(function(){
       $("input#cValue").change(function(e){
             let celsuis = $(e.target).val();
@@ -25,3 +26,19 @@ $(function(){
             $("input#cValue").val(celsuis.toFixed(0))
       })
 })
+// using regular javascript
+
+// document.querySelector("input#cValue").onchange = function(e){
+//       let celsuis = (e.target).value;
+//       let fahrenheit = celsuis*1.8 + 32;
+
+//       document.querySelector("input#fValue").value(fahrenheit.toFixed(0))
+// }
+
+// document.querySelector("input#fValue").onchange = function(e){
+//       let fahrenheit = (e.target).value;
+//       let celsuis = (fahrenheit-32)/1.8;
+
+
+//       document.querySelector("input#cValue").value(celsuis.toFixed(0))
+// }
